c57528c939d8168c70d2d58cc871a17e3387dd98
Mathias <Storm>
2023-10-24 20:03:52 +0200

    chore: integrate vite react app into asp.net core

--- ChooseChase.Web/Program.cs
+++ ChooseChase.Web/Program.cs
@@ -1,12 +1,18 @@
+using ChooseChase.Web.SpaHosting;
+
 var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddControllers();
+builder.Services.AddSpaStaticFiles(configuration => {
+    configuration.RootPath = "WebApp/dist";
+});
 
 var app = builder.Build();
 
 app.UseAuthorization();
 
 app.MapControllers();
+app.UseStaticWebApp();
 
 app.Run();
---
+++ ChooseChase.Web/SpaHosting/WebApplicationExtensions.cs
@@ -0,0 +1,28 @@
+﻿namespace ChooseChase.Web.SpaHosting;
+
+public static class WebApplicationExtensions
+{
+    public static void UseStaticWebApp(this WebApplication app)
+    {
+        const string spaPath = "";
+        app.Map(
+            new PathString(spaPath),
+            client =>
+            {
+                
+                client.UseSpa(spa =>
+                {
+                    if (app.Environment.IsDevelopment())
+                    {
+                        // TODO: Refactor this into configurations
+                        spa.UseProxyToSpaDevelopmentServer("http://localhost:5173");
+                    }
+                    else
+                    {
+                        client.UseSpaStaticFiles();
+                        client.UseSpa(spa => { spa.Options.SourcePath = "WebApp"; });
+                    }
+                });
+            });
+    }
+}
